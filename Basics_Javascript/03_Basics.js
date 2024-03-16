de52b014af88481fbf321f9fb61f1ad2c882fa43
Bibek Thakur <105601993+BibekThakur1@users.noreply.github.com>
2024-03-16 09:48:55 +0000

    string interpolation

---
+++ Basics_Javascript/03_Basics.js
@@ -0,0 +1,34 @@
+// let score=36;
+
+// console.log(typeof score);
+
+// let string1=String(score);
+// console.log(typeof string1);
+
+// //if there exists a number in the changed string then it will give NaN not a number error
+
+// let score1=36;
+
+// console.log(typeof score1);
+
+// let string2=String(score1);
+// console.log(typeof string2);
+// console.log(string2)
+
+
+// let boolVal=" ";
+
+// let Val=Boolean(boolVal)
+// console.log(Val);
+
+// let numb=55;
+
+// let strnumb= String()
+
+let num=5;
+// let num2=-5;
+let num2= -num;
+console.log(num2);
+
+
+console.log()
